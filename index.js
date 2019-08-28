1bfce51c087c6a086c5d32da13d60111a1425f1b
remiconnesson <remiconnesson2@gmail.com>
2019-08-28 12:33:26 +0200

    Webcam integration

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-console.log(faceapi)
+const video = document.getElementById('video')
 
+function startVideo() {
+    navigator.getUserMedia(
+        { video: {} },
+        stream => video.srcObjct = steam,
+        err => console.error(err)
+    )
+}
 
-
+startVideo()
